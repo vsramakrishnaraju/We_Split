e13b1f96c088811025b07bb19e642955ede99686
Venkata K <venkata@Venkatas-MBP.ht.home>
2024-01-05 18:10:16 -0600

    full code

---
+++ WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Venkata K on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
